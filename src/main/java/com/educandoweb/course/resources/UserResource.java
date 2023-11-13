2e1233491a75fab2a3480419fadac6b37050dcbb
Brenda Marcela <brendamarcelasilvestre2126@gmail.com>
2023-11-13 16:17:15 -0300

    User Insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +31,9 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	 }
+	
+	public ResponseEntity<User> insert (@RequestBody User obj) {
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
+	}
 	}
